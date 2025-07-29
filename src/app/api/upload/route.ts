68f427c3867bfe2a89121693a8e18940e87ebef9
Breval <breval2000@live.fr>
2025-07-29 15:08:00 +0200

    feat: update .gitignore and add nixpacks configuration for uploads and avatars

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -26,7 +26,6 @@ export async function POST(req: NextRequest) {
 
     await writeFile(path, buffer);
     console.log(`File saved to ${path}`);
-
     const fileUrl = `/uploads/${sanitizedFilename}`;
     return NextResponse.json({ success: true, fileUrl });
   } catch (error) {
